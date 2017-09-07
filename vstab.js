d5ae16ad06fd9c6eca2c606a1cc0da28098cec4d
JustOff <Off.Just.Off@gmail.com>
2017-09-07 22:56:18 +0300

    Import sources form local repo

---
+++ vstab.js
@@ -0,0 +1,20 @@
+var vstabClass =
+{
+	InterceptVS: function ()
+	{
+		var page = content.location.href;
+		var ioService = Components.classes["@mozilla.org/network/io-service;1"]
+		                .getService(Components.interfaces.nsIIOService);
+		var uri = ioService.newURI(page, null, null);
+		var proto = uri.scheme;
+
+		if (proto != "view-source") {
+			openUILinkIn("view-source:"+uri.spec, "tab");
+		}
+	},
+
+	BrowserViewSourceOfDocument: function (aDocument)
+	{
+		openUILinkIn("view-source:"+aDocument.location.href, "tab");
+	}
+};
